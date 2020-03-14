e5b0774e0573d4da01aee25e04718092321de8b8
Ming Ho <hkboi108@hotmail.com>
2020-03-14 00:34:10 -0700

    Update db config for Heroku

--- db/config.js
+++ db/config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Set up MongoDb connection
-const mongoURI = 'mongodb://localhost:27017/2019coronavirus';
+const mongoURI = process.env.NODE_ENV === 'production' ? `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PW}@ds061711.mlab.com:61711/heroku_gbg72cmc` : 'mongodb://localhost:27017/2019coronavirus';
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
