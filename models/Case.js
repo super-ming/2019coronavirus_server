ba0111abf4d5e141c1b84add1be7d897ed22ed74
Ming Ho <hkboi108@hotmail.com>
2020-04-13 19:37:36 -0700

    update case model schema to map new csv columns

--- models/Case.js
+++ models/Case.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CaseSchema = new Schema({
+    'city': String,
     'province': String,
     'country': String,
     'lastUpdate': String,
+    'lat': Number,
+    'lng': Number,
     'confirmedCount': Number,
     'deathCount': Number,
     'recoveredCount': Number,
-    'lat': Number,
-    'lng': Number
+    'activeCount': Number,
+    'key': String
 }, {autoCreate: true});
 
 module.exports = mongoose.model('Case', CaseSchema);
